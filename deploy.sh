4d1ffa7da1953f8b7589e8c030eedf1b690966f7
StefanV1sser <stefan.visser@apm-ecampus.de>
2021-07-15 20:13:08 +0200

    chore: create deploy script and set publicPath

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+git init
+git add -A
+git commit -m 'chore: Deploy'
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:DutchGerman/quartier-quartett.git master:gh-pages
+
+cd -
+
