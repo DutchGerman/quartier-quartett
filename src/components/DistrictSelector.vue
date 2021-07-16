914a12a5e6ffb5831f2079428eb4f0dafa02a9dc
StefanV1sser <stefan.visser@apm-ecampus.de>
2021-07-16 14:12:45 +0200

    feat: landing page + district selector

---
+++ src/components/DistrictSelector.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <select @change="setDistrict()" v-model="value">
+      <option :value="null" :selected="!value">
+        Zufällig
+      </option>
+      <option 
+        v-for="district in districts" 
+        :key="district.id" 
+        :value="district.id"
+        :selected="district.id === $store.state.district"
+        >
+        {{ district.name }}
+      </option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'DistrictSelector',
+  data () {
+    return {
+      value: null,
+      districts: [
+        { name: 'Innenstadt', id: 1 },
+        { name: 'Weststadt', id: 2 },
+        { name: 'Westerberg', id: 3 },
+        { name: 'Eversburg', id: 4 },
+        { name: 'Hafen', id: 5 },
+        { name: 'Sonnenhügel', id: 6 },
+        { name: 'Haste', id: 7 },
+        { name: 'Dodesheide', id: 8 },
+        { name: 'Gartlage', id: 9 },
+        { name: 'Schinkel', id: 10 },
+        { name: 'Widukindland', id: 11 },
+        { name: 'Schinkel-Ost', id: 12 },
+        { name: 'Fledder', id: 13 },
+        { name: 'Schölerberg', id: 14 },
+        { name: 'Kalkhügel', id: 15 },
+        { name: 'Wüste', id: 16 },
+        { name: 'Sutthausen', id: 17 },
+        { name: 'Hellern', id: 18 },
+        { name: 'Atter', id: 19 },
+        { name: 'Pye', id: 20 },
+        { name: 'Darum/Gretesch/Lüstringen', id: 21 },
+        { name: 'Voxtrup', id: 22 },
+        { name: 'Nahne', id: 23 }
+      ]
+    }
+  },
+  methods: {
+    setDistrict(val) {
+      this.$store.commit('changeDistrict', this.value)
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+select {
+  height: 32px;
+  font-size: 14px;
+  width: 100%;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="home">
     <box class="start">
-      Hello
+      <h2>OS-City-Quartett</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut euismod hendrerit justo, nec pretium quam condimentum vel. Pellentesque id auctor nunc, in scelerisque nunc. Maecenas vitae pharetra mauris. Curabitur dignissim, tellus vel bibendum dictum, nunc lectus consectetur leo, in pulvinar augue felis at velit. Cras.</p>
+      <div class="district-grid">
+        <span>Stadtteil</span>
+        <district-selector class="district" />
+      </div>
+      <div class="button-wrapper">
+        <router-link class="start-button" :to="{ name: 'Play' }">LOS!</router-link>
+      </div>
     </box>
   </div>
 </template>
@@ -9,16 +17,26 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import Box from '@/components/Box.vue';
+import DistrictSelector from '@/components/DistrictSelector.vue';
 
 @Component({
   components: {
-    Box
+    Box,
+    DistrictSelector
   },
 })
 export default class Home extends Vue {}
 </script>
 
 <style lang="css" scoped>
+h2 {
+  background-color: #c44536;
+  color: #fff;
+  margin: 0;
+  padding: 15px;
+  text-align: center;
+}
+
 .home {
   width: 100%;
   height: 100%;
@@ -31,5 +49,41 @@ export default class Home extends Vue {}
   width: 660px;
   height: auto;
   min-height: 400px;
+}
+
+.start p {
+  padding: 0 25px;
+  text-align: justify;
+  line-height: 30px;
+}
+
+.district-grid {
+  padding: 0 25px;
+}
+
+.district-grid span {
+  font-weight: bold;
+  display: inline-block;
+  margin-bottom: 7px;
+}
+
+.button-wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 25px;
+}
+
+.start-button {
+  background-color: #ffb41f;
+  padding: 5px 30px;
+  color: #fff;
+  font-size: 48px;
+  font-family: 'Rubik', sans-serif;
+  text-decoration: none;
+}
+
+.start-button:hover {
+  background-color: rgb(209, 151, 36);
 }
 </style>
